a285d64120846547d092213aec4c851e2562f204
qiyuan Huang <18845976159@163.com>
2026-04-27 01:03:45 +0800

    fix：调整仿真launch文件

--- env_ws/start_shell.sh
+++ env_ws/start_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "************************** 启动仿真环境 **************************"
 source install/setup.bash
-ros2 launch vehicle_simulator system_indoor.launch
+ros2 launch vehicle_simulator system_common.launch.py
